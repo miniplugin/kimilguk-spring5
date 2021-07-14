c8e605558a01314443a1f0fdbfaebdf375eb48c4
boramcom@daum.net <boramcom@daum.net>
2021-07-14 16:48:21 +0900

    7교시 커밋

--- src/main/java/com/edu/controller/LoginController.java
+++ src/main/java/com/edu/controller/LoginController.java
@@ -86,7 +86,7 @@ public class LoginController {
 			session.setAttribute("session_userid", useremail);
 			session.setAttribute("session_levels", "ROLE_USER");
 			session.setAttribute("session_username", username);
-			
+			session.setAttribute("session_login_type", "sns");//마이페이지 않보이게처리용
 			rdat.addFlashAttribute("msg", "네이버 아이디 로그인");//출력결과: 네이버 아이디 로그인 이(가) 성공 하였습니다. alert창으로 나옴
 		} else {
 			rdat.addFlashAttribute("msgError", "네이버 인증이 실패했습니다. 다시 로그인해 주세요!");
--- src/main/webapp/WEB-INF/views/home/include/header.jsp
+++ src/main/webapp/WEB-INF/views/home/include/header.jsp
@@ -72,7 +72,9 @@ if("${msgError}" != "") {
 						<!-- 로그인 후 보이는 메뉴(아래) -->
 						<li><a href="#">${session_username} 님 환영합니다.</a></li>
 						<li><a href="/logout">로그아웃</a></li>
+						<c:if test="${session_login_type ne 'sns'}">
 						<li><a href="/member/mypage_form">마이페이지</a></li>
+						</c:if>						
 						<!-- ROLE_ADMIN 권한만 AdminLTE에 가능하게 조건추가 -->
 						<c:if test="${session_levels eq 'ROLE_ADMIN'}">
 							<li><a href="/admin">AdminLTE</a></li>
